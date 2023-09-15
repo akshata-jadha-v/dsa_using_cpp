ec4c779245990379eef4d40966c746e5c388fe6e
akshata-jadha-v <142650428+akshata-jadha-v@users.noreply.github.com>
2023-09-16 00:32:02 +0530

    added marks program

---
+++ marks.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+    cin>>ch;
+    cout<<"here:"<<ch<<endl;
+    int marks;
+    cout<<"Enter the marks here:"<<endl;
+    // take input from user
+    cin>>marks;
+    cout<<"Printing marks:"<<marks<<endl;
+}
