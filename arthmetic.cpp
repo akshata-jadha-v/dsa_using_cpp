6e742096e38d6cae2a2c35587f16403f1410e628
akshata-jadha-v <142650428+akshata-jadha-v@users.noreply.github.com>
2023-09-15 21:01:34 +0530

    added arthmetic.cpp

---
+++ arthmetic.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;    
+
+int main() 
+{
+    int a=5;
+    int b=10;
+
+    cout<<a+b<<endl;
+    cout<<a-b<<endl;
+    cout<<a*b<<endl;
+    cout<<b/a<<endl;
+    cout<<b%a<<endl;
+
+    return 0;
+}
